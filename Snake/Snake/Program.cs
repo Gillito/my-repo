f802f6de80b5017edf1c7367b764929b69a7dd98
Gillito <letsrocksc2@inbox.ru>
2016-01-12 08:08:26 +0200

    List added

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,13 +11,17 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-
             Point p3 = new Point(7, 7, '$');
-            p3.Draw();
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+
+            foreach (Point item in pList)
+                item.Draw();
+
 
             Console.ReadLine();
         }
