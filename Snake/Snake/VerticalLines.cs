38a14d2d51f51cfc227ae633d8309cd904311cdd
Gillito <letsrocksc2@inbox.ru>
2016-01-12 11:37:39 +0200

    classes chenge

---
+++ Snake/Snake/VerticalLines.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLines : Figure
+    {
+        public VerticalLines(int yTop, int yBottom, int x, char sym)
+        {
+            hLines = new List<Point>();
+
+            for (int i = yTop; i <= yBottom; i++)
+            {
+                Point p = new Point(x, i, sym);
+                hLines.Add(p);
+            }
+        }
+    }
+}
